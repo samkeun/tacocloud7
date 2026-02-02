013da9b5f477002da2d42fed6f6be35872ee182b
samkeun <skimok@gmail.com>
2026-02-02 20:14:54 +0900

    update

--- src/main/java/tacos/User.java
+++ src/main/java/tacos/User.java
@@ -1,6 +1,5 @@
 package tacos;
 import jakarta.persistence.*;
-import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,7 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Arrays;
 import java.util.Collection;
 
-@Data
 @Document(collection = "users")
 public class User implements UserDetails {
 
@@ -21,13 +19,15 @@ public class User implements UserDetails {
 
     private String username;
     private String password;
-    @Getter  // 이것만 추가!
     private String fullname;
     private String street;
     private String city;
     private String state;
     private String zip;
     private String phoneNumber;
+
+    // 기본 생성자 (MongoDB 필수)
+    public User() {}
 
     public User(String username, String password, String fullname,
                 String street, String city, String state,
@@ -42,19 +42,86 @@ public class User implements UserDetails {
         this.phoneNumber = phoneNumber;
     }
 
+    // Getter/Setter
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // UserDetails 인터페이스 구현
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
-    public String getUsername() { return username; }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     @Override
-    public String getPassword() { return password; }
+    public String getPassword() {
+        return password;
+    }
 
-//    @Override
-//    public String getFullname() { return fullname; }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Override
     public boolean isAccountNonExpired() {
@@ -75,5 +142,4 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
-
 }
